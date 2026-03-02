8b82a2844a2d65b2e37c9e802c4fa2cef28a002a
Juan Carlos Imbernon <jcimbernon@alu.ucam.edu>
2026-03-02 20:28:03 +0100

    fix: resolver conflicto con App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./pages/Home";
-import Login from "./pages/Login"; 
-import RecuperarPassword from "./pages/RecuperarPassword"; 
 import Chat from "./pages/Chat";
 import Ranking from "./pages/Ranking";
 import Juegos from "./pages/Juegos";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import RecuperarPassword from "./pages/RecuperarPassword";
+import Gameplay from "./pages/GamePlay";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
+      <Routes>
+        {/* Auth */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/recuperar-password" element={<RecuperarPassword />} />
+
+        {/* Main */}
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/recuperar-password" element={<RecuperarPassword />} />
           <Route path="/juegos" element={<Juegos />} />
           <Route path="/ranking" element={<Ranking />} />
           <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </Layout>
+          <Route path="/juego/:id/gameplay" element={<Gameplay />} />
+        </Route>
+
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
